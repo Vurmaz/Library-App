2b02e39379e1e23e12311da06ab929c17e64245f
Vurmaz <sadikvurmaz@gmail.com>
2022-01-17 14:50:19 +0300

    Final Changes

--- script.js
+++ script.js
@@ -9,11 +9,8 @@ const submit = document.querySelector("#SUBMİT")
 const form = document.querySelector("form")
 const body = document.querySelector(".bodi");
 
-
-
 let STORAGE_LİBRARY = "library.storage";
 let STORAGE_BTN_COLOR = "library.color";
-
 
 let library = JSON.parse(localStorage.getItem(STORAGE_LİBRARY)) || [] ;
 let read = localStorage.getItem(STORAGE_BTN_COLOR)
@@ -21,52 +18,39 @@ let read = localStorage.getItem(STORAGE_BTN_COLOR)
 function render () {
 clearElement(container);
 library.forEach((e)=>{
-
-let book = document.createElement("div");
+const book = document.createElement("div");
 book.classList.add("books");
 book.dataset.listId = e.id;
 
-let bookName = document.createElement("div");
+const bookName = document.createElement("div");
 bookName.textContent = `${e.name}`
-bookName.style.textAlign = "center"
-bookName.style.padding = "15px";
 
-let labelName = document.createElement("label");
+const labelName = document.createElement("label");
 labelName.textContent = "Book Name";
 labelName.classList.add("book-label")
 
-
-let authorName = document.createElement("div");
+const authorName = document.createElement("div");
 authorName.textContent = `${e.author}`
-authorName.style.textAlign = "center"
-authorName.style.padding = "15px";
-let labelAuthor = document.createElement("label");
+
+const labelAuthor = document.createElement("label");
 labelAuthor.textContent = "Author Name";
 labelAuthor.classList.add("book-label");
 
-
-let totalPage = document.createElement("div");
+const totalPage = document.createElement("div");
 totalPage.textContent = `${e.page}`;
-totalPage.style.textAlign = "center"
-totalPage.style.padding = "15px";
-let labelPage = document.createElement("label");
+
+const labelPage = document.createElement("label");
 labelPage.textContent = "Total Page";
 labelPage.classList.add("book-label");
 
-
-let bookRead = document.createElement("button");
+const bookRead = document.createElement("button");
 bookRead.classList.add('btn-books')
 bookRead.innerText = 'Not Read'
-bookRead.style.marginLeft = "19%";
-bookRead.style.width = "130px"
 check(book,bookRead)
 
-let removeBtn = document.createElement("button");
+const removeBtn = document.createElement("button");
 removeBtn.classList.add('btn-books')
-removeBtn.style.marginLeft = "19%";
 removeBtn.textContent = "REMOVE";
-removeBtn.style.marginTop = "13px";
-removeBtn.style.width = "130px"
 
 container.appendChild(book);
 book.appendChild(labelName)
@@ -78,33 +62,19 @@ book.appendChild(totalPage)
 book.appendChild(bookRead)
 book.appendChild(removeBtn)
 save()
-
 bookReadHandle(book,bookRead)
 
-
-
-console.log(library)
 removeBtn.addEventListener("click",()=>{
 	let removingItem = library.findIndex(item=>item.id === book.dataset.listId);
 	container.removeChild(removeBtn.parentNode);
 	library.splice(removingItem,removingItem + 1);
-
 	save()
-
 })
-
 })
-
-
-
 }
-
-function save(e) {
+function save() {
 	localStorage.setItem(STORAGE_LİBRARY,JSON.stringify(library));
-	//localStorage.setItem(STORAGE_BTN_COLOR,e)
-
 }
-
 function clearElement(elem) {
 while(elem.firstChild){
 	elem.removeChild(elem.firstChild)
@@ -117,35 +87,25 @@ function bookReadHandle(book,bookRead) {
 			item.read ? item.read = false : item.read = true
 			check(book,bookRead)
 			}
-
 		})
-		
 	})}
-	function check(book,bookRead) {
-		library.forEach((item)=>{
-			if(item.id == book.dataset.listId){
-				if (item.read === false) {
-				bookRead.innerText = "Not Read";
-				bookRead.classList.remove("light-color")
-				bookRead.classList.add("grey-color") 
-			
+function check(book,bookRead) {
+	library.forEach((item)=>{
+		if(item.id == book.dataset.listId){
+			if (item.read === false) {
+			bookRead.innerText = "Not Read";
+			bookRead.classList.remove("light-color")
+			bookRead.classList.add("grey-color") 
 			}
-			else if(item.read === true){
-				bookRead.innerText = "Read"
-				bookRead.classList.remove("grey-color")
-				bookRead.classList.add("light-color") 
-
+		else if(item.read === true){
+			bookRead.innerText = "Read"
+			bookRead.classList.remove("grey-color")
+			bookRead.classList.add("light-color") 
 			}
-			}
-				
-			save()
-			
+			}	
+			save()	
 		})
 	}
-
-
-
-
 form.addEventListener("submit",(e)=>{
 	e.preventDefault();
 	if (name.value.length > 0 && author.value.length > 0 && page.value > 0 ) {
@@ -155,30 +115,19 @@ form.addEventListener("submit",(e)=>{
 			author : author.value,
 			page : page.value,
 			read : false,
-			
-		});
-
+		})
 		pop.classList.remove("pop-up-active");
 		overlay.classList.remove("overlay-active")
-
-
-
 		name.value = "";
 		author.value = "";
 		page.value = "";
 	}
-
-
-
 save();
 render();
 })
 window.addEventListener("load",()=>{
-	render()
-	
-	
+	render()	
 })
-
 addBookBtn.addEventListener("click",()=>{
 	pop.classList.add("pop-up-active");
 	overlay.classList.add("overlay-active")
